002c65c50ab5d2234cdd214c0664878e9317eff9
mtoslalibu <44147008+mtoslalibu@users.noreply.github.com>
2022-05-16 09:45:21 -0400

    Create AstraeaController.py

---
+++ src/AstraeaController.py
@@ -0,0 +1,13 @@
+# Astraea main program
+  
+  
+print("***** Welcome to Astraea!")
+  
+# Defining main function
+def main():
+    print("----")
+  
+  
+# __name__
+if __name__=="__main__":
+    main()
