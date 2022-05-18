e06d35654f5009e074ea32f052ad8a2c79382776
Mert Toslali <merttoslali@Merts-mac.local>
2022-05-18 12:51:23 -0400

    Fix logging statements

--- src/AstraeaControllerEval.py
+++ src/AstraeaControllerEval.py
@@ -55,7 +55,7 @@ class AstraeaControllerEval():
         # If folder doesn't exist, then create it.
         if not CHECK_FOLDER:
             os.makedirs(resultDir)
-            logger.info("created folder : ", resultDir)
+            logger.info(str(("created folder : ", resultDir)))
 
         # The data assigned to the list e.g., list_data=[['03','Smith','Science'], ...]
         # First, open the old CSV file in append mode, hence mentioned as 'a'
@@ -76,7 +76,7 @@ class AstraeaControllerEval():
 
         # Astraea framework Initialized
         experimentID = "Experiment-{}-{}".format(problem_now, random.randint(0,99))
-        logger.info("---- Astraea {} evaluator started!".format(experimentID))   
+        logger.info(str(("---- Astraea {} evaluator started!".format(experimentID))   ))
 
         bandit = banditalg.ABE("ABE", experimentID, confidence=confidence, reward_field = reward_field, elim_percentile = elim_percentile)
         astraeaOrc = ao.AstraeaOrc()
@@ -87,14 +87,14 @@ class AstraeaControllerEval():
         epoch = 0
 
         totalEpoch = int(totalExpDuration/period)
-        logger.info("\n---- Will run for total of epoch : ", totalEpoch)
+        logger.info(str(("---- Will run for total of epoch : ", totalEpoch)))
 
         while epoch < totalEpoch:
             epoch += 1
-            logger.info("\n\n\n---- runninng epoch: ", epoch)
+            logger.info(str(("---- runninng epoch: ", epoch)))
 
             all_traces = astraeaMan.get_traces_jaeger_api(service = "compose-post-service")
-            logger.info("collected the batch with len: ", len(all_traces["data"]))
+            logger.info(str(("collected the batch with len: ", len(all_traces["data"]))))
 
 
             ## parse traces and extract span units
@@ -114,7 +114,7 @@ class AstraeaControllerEval():
             # astraeaOrc.issue_sampling_policy(splits)
             astraeaOrc.issue_sampling_policy_txt(splits)
 
-            logger.info("Finished epoch ", epoch)
+            logger.info(str(("Finished epoch ", epoch)))
 
 
             ### collect stats
@@ -124,7 +124,7 @@ class AstraeaControllerEval():
                 span_counts.append([len(trace["spans"]), epoch])
 
             self.append_to_csv(resultDir, "{}/{}-tracesizes.csv".format(resultDir, experimentID), span_counts)
-            logger.info("******* Saved trace sizes with mean ", np.mean(span_counts, axis=0))
+            logger.info(str(("******* Saved trace sizes with mean ", np.mean(span_counts, axis=0))))
 
 
             ### sampling policy
@@ -134,7 +134,7 @@ class AstraeaControllerEval():
                     name, var = line.partition(" ")[::2]
                     sampling_policies.append([name.strip(), float(var), epoch])
                     if name.strip() == problem_now:
-                        logger.info("******* Problem's sampling policy ",name.strip(), " : ", var)
+                        logger.info(str(("******* Problem's sampling policy ",name.strip(), " : ", var)))
             
 
             self.append_to_csv(resultDir, "{}/{}-probability.csv".format(resultDir, experimentID), sampling_policies)
